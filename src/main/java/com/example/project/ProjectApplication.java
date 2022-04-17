280fb3d89c7086703aa0ea1222fb7365bda67b18
lintianchi <544204981@qq.com>
2022-04-17 19:38:55 +0800

    1

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -3,9 +3,10 @@ package com.example.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@EnableJms
 @SpringBootApplication
 public class ProjectApplication {
 
--- src/test/java/com/example/project/ProjectApplicationTests.java
+++ src/test/java/com/example/project/ProjectApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.project;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
